9e7ec2e139fe770cef81f436aa567ec8f9ace25c
matiaslopez23 <matiaslopezcremasco@gmail.com>
2025-08-06 11:52:17 +0000

    trese bimestre

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-class class:
-    def __init__(self, atributo):
-        self. atributo = atributo
-        
+class CLase:
+    def __init__(self , atributo
+                 ):
+     self.atributo = atributo
+
+     def metodo(self)
+        return "el atributo es: " = self.atributo
+    
+    def imprimir(self)
+       print("el atributo es" = self.atributo )
+
+       instancia = CLase("valor")
+       print(instancia.metodo())
+       instancia.imprimir()

bc4912997d15e1e0330ff8a8976558e5e2023464
matiaslopez23 <matiaslopezcremasco@gmail.com>
2025-09-03 11:53:04 +0000

    03/09/25

--- suma.py
+++ suma.py
@@ -1,12 +1,2 @@
-numero1=3
-numero2=4
-suma=(numero1 + numero2)
-print(suma)
-# menor <
-#mayor >
-#igual ==
-#print("tal es mayor que tal",suma)
-if numero1 > numero2:  
-    print("numero1 > numero2")
-if numero1 < numero2:  
-    print("numero1 < numero2")
+class claase:
+      d
